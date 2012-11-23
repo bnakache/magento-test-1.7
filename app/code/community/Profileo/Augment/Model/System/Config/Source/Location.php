961e3c70ea77e887011f8ba6f33bdfb6f2c40e5c
Ben <benjamin@profileo.com>
2012-11-23 18:27:27 +0100

    Revert "Augment Dev Module Beta 2"
    This reverts commit 5413e43e8d64f4be216173926423f5e14f11564f.

--- app/code/community/Profileo/Augment/Model/System/Config/Source/Location.php
+++ app/code/community/Profileo/Augment/Model/System/Config/Source/Location.php
@@ -9,8 +9,8 @@ class Profileo_Augment_Model_System_Config_Source_Location
     {
 		return array(
 		  array('value' => 0, 'label' => Mage::helper('augment')->__('Local Server')),
-		  array('value' => 1, 'label' => Mage::helper('augment')->__('AugmentDev Server: Use your Product Ref./SKU')),
-//		  array('value' => 2, 'label' => Mage::helper('augment')->__('AugmentDev Server: Product ID')),		  
+		  array('value' => 1, 'label' => Mage::helper('augment')->__('AugmentDev Server: Product ID')),
+		  array('value' => 2, 'label' => Mage::helper('augment')->__('AugmentDev Server: Product Reference')),
 		);
     }
 }
